ffaec5dbc861b32f5ed9e4f79d3dfa30b044a513
Igor Cieply <igorcieply@gmail.com>
2023-02-07 23:22:09 -0600

    improved controls system
    Moved all controller-related code into its own file to cut down on robot container file size and in order to make dealing with controls less painful. All buttons now have nice, self-explanatory variable names.

---
+++ src/main/java/frc/robot/Controls.java
@@ -0,0 +1,79 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.Extender;
+import frc.robot.commands.BrakeCmd;
+import frc.robot.commands.SetOrientationCmd;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ExtenderSubsystem;
+import frc.robot.subsystems.FourBarSubsystem;
+import frc.robot.subsystems.GrabberSubsystem;
+
+/* Controller buttons are created and their actions are defined. */
+public class Controls {
+    // References to subsystems.
+    private final DriveSubsystem m_DriveSubsystem;
+    private final ExtenderSubsystem m_ExtenderSubsystem;
+    private final FourBarSubsystem m_FourBarSubsystem;
+    private final GrabberSubsystem m_GrabberSubsystem;
+
+    private final XboxController driverController = new XboxController(RobotMap.kDriverControllerPort);
+    private final XboxController operatorController = new XboxController(RobotMap.kDriverControllerPort);
+
+    private final JoystickButton dA = new JoystickButton(driverController, 1);
+    private final JoystickButton dB = new JoystickButton(driverController, 2);
+    private final JoystickButton dX = new JoystickButton(driverController, 3);
+    private final JoystickButton dY = new JoystickButton(driverController, 4);
+    private final JoystickButton dLeftBumper = new JoystickButton(driverController, 5);
+    private final JoystickButton dRightBumper = new JoystickButton(driverController, 6);
+    private final JoystickButton dBack = new JoystickButton(driverController, 7);
+    private final JoystickButton dStart = new JoystickButton(driverController, 8);
+    private final JoystickButton dLStickDown = new JoystickButton(driverController, 9);
+    private final JoystickButton dRStickDown = new JoystickButton(driverController, 10);
+    private final Trigger dLeftTrigger = new Trigger(() -> driverController.getLeftTriggerAxis() >= 0.1);
+    private final Trigger dRightTrigger = new Trigger(() -> driverController.getRightTriggerAxis() >= 0.1);
+    private final POVButton dDPadUp = new POVButton(driverController, 0);
+    private final POVButton dDPadRight = new POVButton(driverController, 90);
+    private final POVButton dDPadDown = new POVButton(driverController, 180);
+    private final POVButton dDPadLeft = new POVButton(driverController, 270);
+
+    private final JoystickButton oA = new JoystickButton(operatorController, 1);
+    private final JoystickButton oB = new JoystickButton(operatorController, 2);
+    private final JoystickButton oX = new JoystickButton(operatorController, 3);
+    private final JoystickButton oY = new JoystickButton(operatorController, 4);
+    private final JoystickButton oLeftBumper = new JoystickButton(operatorController, 5);
+    private final JoystickButton oRightBumper = new JoystickButton(operatorController, 6);
+    private final JoystickButton oBack = new JoystickButton(operatorController, 7);
+    private final JoystickButton oStart = new JoystickButton(operatorController, 8);
+    private final JoystickButton oLStickDown = new JoystickButton(operatorController, 9);
+    private final JoystickButton oRStickDown = new JoystickButton(operatorController, 10);
+    private final Trigger oLeftTrigger = new Trigger(() -> operatorController.getLeftTriggerAxis() >= 0.1);
+    private final Trigger oRightTrigger = new Trigger(() -> operatorController.getRightTriggerAxis() >= 0.1);
+    private final POVButton oDPadUp = new POVButton(operatorController, 0);
+    private final POVButton oDPadRight = new POVButton(operatorController, 90);
+    private final POVButton oDPadDown = new POVButton(operatorController, 180);
+    private final POVButton oDPadLeft = new POVButton(operatorController, 270);
+
+    public Controls(DriveSubsystem driveSubsystem, ExtenderSubsystem extenderSubsystem, 
+                    FourBarSubsystem fourBarSubsystem, GrabberSubsystem grabberSubsystem) {
+        m_DriveSubsystem = driveSubsystem;
+        m_ExtenderSubsystem = extenderSubsystem;
+        m_FourBarSubsystem = fourBarSubsystem;
+        m_GrabberSubsystem = grabberSubsystem;
+
+        // Driver controls.
+        dLeftBumper.onTrue(new SetOrientationCmd(m_DriveSubsystem));
+        dRightBumper.onTrue(new BrakeCmd(m_DriveSubsystem));
+    }
+
+    public XboxController getDriverController() {
+        return driverController;
+    }
+
+    public XboxController getOperatorController() {
+        return operatorController;
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,8 @@
 
 package frc.robot;
 
-import frc.robot.Constants.Controller;
-import frc.robot.commands.BrakeCmd;
+import frc.lib.auto.Trajectories;
 import frc.robot.commands.DriveCmd;
-import frc.robot.commands.SetOrientationCmd;
 import frc.robot.commands.auto.TestOneAuto;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ExtenderSubsystem;
@@ -15,12 +13,9 @@ import frc.robot.subsystems.FourBarSubsystem;
 import frc.robot.subsystems.GrabberSubsystem;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotContainer {
     private final DriveSubsystem m_DriveSubsystem;
@@ -28,12 +23,11 @@ public class RobotContainer {
     private final FourBarSubsystem m_FourBarSubsystem;
     private final GrabberSubsystem m_GrabberSubsystem;
 
+    private final Controls controls;
+
     private final Trajectories paths;
     private final SendableChooser<Command> routineChooser;
     private final TestOneAuto testRoutine1;
-
-    private final XboxController driveController;
-    private final XboxController operatorController;
 
     public RobotContainer() {
         m_DriveSubsystem = new DriveSubsystem();
@@ -41,8 +35,8 @@ public class RobotContainer {
         m_FourBarSubsystem = new FourBarSubsystem();
         m_GrabberSubsystem = new GrabberSubsystem();
 
-        driveController = new XboxController(Controller.kDriverPort);
-        operatorController = new XboxController(Controller.kOperatorPort);
+        controls = new Controls(m_DriveSubsystem, m_ExtenderSubsystem, 
+                                m_FourBarSubsystem, m_GrabberSubsystem);
 
         configureBindings();
 
@@ -56,23 +50,11 @@ public class RobotContainer {
         routineChooser.addOption("Test Routine 1", testRoutine1.getCommand());
         SmartDashboard.putData("Auto Routines", routineChooser);
 
-        m_DriveSubsystem.setDefaultCommand(new DriveCmd(m_DriveSubsystem, () -> driveController.getLeftY(),
-                () -> driveController.getLeftX(), () -> driveController.getRightX()));
+        m_DriveSubsystem.setDefaultCommand(new DriveCmd(m_DriveSubsystem, () -> controls.getDriverController().getLeftY(),
+                () -> controls.getDriverController().getLeftX(), () -> controls.getDriverController().getRightX()));
     }
 
-    private void configureBindings() {
-        Trigger drLeftTrigger = new Trigger(() -> driveController.getLeftTriggerAxis() >= 0.1);
-        Trigger drRightTrigger = new Trigger(() -> driveController.getRightTriggerAxis() >= 0.1);
-
-        Trigger opLeftTrigger = new Trigger(() -> operatorController.getLeftTriggerAxis() >= 0.1);
-        Trigger opRightTrigger = new Trigger(() -> operatorController.getRightTriggerAxis() >= 0.1);
-
-        // Driver.
-        new JoystickButton(driveController, XboxController.Button.kLeftBumper.value)
-            .onTrue(new SetOrientationCmd(m_DriveSubsystem));
-        new JoystickButton(driveController, XboxController.Button.kRightBumper.value)
-            .onTrue(new BrakeCmd(m_DriveSubsystem));
-    }
+    private void configureBindings() {  }
 
     public Command getAutonomousCommand() {
         m_DriveSubsystem.resetOdometry(new Pose2d());
