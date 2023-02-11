7f69a6e6d96de01c89c807ff6c45be90ba09c684
Igor Cieply <igor.a.cieply@gmail.com>
2023-02-11 16:52:59 -0600

    added positionial subsystem
    Cuts down on the amount of commands by using inheritance.

---
+++ src/main/java/frc/robot/subsystems/PositionalSubsystem.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PositionalSubsystem extends SubsystemBase {
+    public void run() {  }
+    public void changePosition() {  }
+}
+
+
